37bad1b3681f78e9799544449282aca27e00089a
Duncan Skinner <duncanskinner94@gmail.com>
2019-09-11 20:06:43 +0800

    Using center method to format gets

--- directory.rb
+++ directory.rb
@@ -19,7 +19,16 @@ def print_header
 end
 # # we print the list of students
 def print(names)
-  names.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)" }
+  # count = 1
+  names.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) - #{student[:degree]}, #{student[:nationality]}, #{student[:height]}" }
+  # until count == names.length do
+  #   if names.empty?
+  #     break
+  #   else
+  #     puts "#{name} (#{names[:cohort]} cohort)"
+  #   end
+  # count += 1
+  # end
 end
 # # we print the total number of students
 def print_footer(names)
@@ -27,23 +36,32 @@ def print_footer(names)
 end
 # # finally, we print the methods out to run the program
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(50, "--------")
+  puts "To finish, just hit return four times".center(50, "--------")
   # creates an empty array
   students = []
   # gets the first names
   name = gets.chomp
+  puts "\n" + "Please enter the Major of the student".center(50, "--------")
+  degree = gets.chomp
+  puts "\n" + "Please enter the Nationality of the student".center(50, "--------")
+  nationality = gets.chomp
+  puts "\n" + "Please enter the height of the student in cm".center(50, "--------")
+  height = gets.chomp
   # while teh name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    if name.start_with?("D") && name.length < 12  
-      students << {name: name, cohort: :november}
+    if name.start_with?("D") && name.length < 12
+      students << {name: name, cohort: :november, degree: degree, nationality: nationality, height: height }
       puts "Now we have #{students.count} students"
     # get another name from the user
     else
       nil
     end
     name = gets.chomp
+    degree = gets.chomp
+    nationality = gets.chomp
+    height = gets.chomp
   end
   # return the array of students
   students
