5c76d2d605ba13df7be8b7b3d2d5dd73b5bc8b75
Duncan Skinner <duncanskinner94@gmail.com>
2019-09-12 15:22:28 +0800

    Adding more user-inputs

--- directory.rb
+++ directory.rb
@@ -1,34 +1,12 @@
-# student_count = 11
-# # first we create an array of the students
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
+
 # # we print the purpose of the program as a header
 def print_header
   puts "The students of Villains Academy"
-  puts "-------------"
+  puts "================================"
 end
 # # we print the list of students
 def print(names)
-  # count = 1
-  names.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) - #{student[:degree]}, #{student[:nationality]}, #{student[:height]}" }
-  # until count == names.length do
-  #   if names.empty?
-  #     break
-  #   else
-  #     puts "#{name} (#{names[:cohort]} cohort)"
-  #   end
-  # count += 1
-  # end
+  names.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) - #{student[:nationality]}, #{student[:height]}cm" }
 end
 # # we print the total number of students
 def print_footer(names)
@@ -36,32 +14,44 @@ def print_footer(names)
 end
 # # finally, we print the methods out to run the program
 def input_students
-  puts "Please enter the names of the students".center(50, "--------")
-  puts "To finish, just hit return four times".center(50, "--------")
+  puts "Please enter the names of the students".center(50, "******")
+  puts "To finish, just hit return four times".center(50, "******")
   # creates an empty array
   students = []
   # gets the first names
   name = gets.chomp
-  puts "\n" + "Please enter the Major of the student".center(50, "--------")
-  degree = gets.chomp
-  puts "\n" + "Please enter the Nationality of the student".center(50, "--------")
-  nationality = gets.chomp
-  puts "\n" + "Please enter the height of the student in cm".center(50, "--------")
-  height = gets.chomp
   # while teh name is not empty, repeat this code
   while !name.empty? do
+    cohort_dates = [:january, :march, :june, :august, :november]
+
+    puts "\n" + "Please enter the cohort of the student".center(50, "--------")
+    cohort = gets.chomp.downcase.to_sym
+
+    while !cohort_dates.include?(cohort) do
+      puts "Please re-enter cohort of student"
+      cohort = gets.chomp.downcase.to_sym
+    end
+
+    puts "\n" + "Please enter the Nationality of the student".center(50, "--------")
+    nationality = gets.chomp
+
+    if nationality.empty?
+      nationality = "British"
+    end
+
+    puts "\n" + "Please enter the height of the student in cm".center(50, "--------")
+    height = gets.chomp
+
     # add the student hash to the array
     if name.start_with?("D") && name.length < 12
-      students << {name: name, cohort: :november, degree: degree, nationality: nationality, height: height }
+      students << {name: name, cohort: cohort, nationality: nationality, height: height }
       puts "Now we have #{students.count} students"
     # get another name from the user
     else
       nil
     end
+    puts "\n" + "Please enter the names of the student".center(50, "******")
     name = gets.chomp
-    degree = gets.chomp
-    nationality = gets.chomp
-    height = gets.chomp
   end
   # return the array of students
   students
