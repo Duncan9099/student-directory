004bcb46ced9e39064fa1b42d4ef3d8ad4469a71
Duncan Skinner <duncanskinner94@gmail.com>
2019-09-13 11:45:05 +0800

    Adding an interactive menu

--- directory.rb
+++ directory.rb
@@ -1,30 +1,25 @@
-
-# # we print the purpose of the program as a header
 def print_header
   puts "The students of Villains Academy"
   puts "================================"
 end
-# # we print the list of students
-def print(names)
-  names.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) - #{student[:nationality]}, #{student[:height]}cm" }
+
+def print_student_list
+  @students.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) - #{student[:nationality]}, #{student[:height]}cm" }
 end
-# # we print the total number of students
-def print_footer(names)
-  if names.count == 1
-    puts "Overall we have #{names.count} great student"
+
+def print_footer
+  if @students.count == 1
+    puts "Overall we have #{@students.count} great student"
   else
-    puts "Overall we have #{names.count} great students"
+    puts "Overall we have #{@students.count} great students"
   end
 end
-# # finally, we print the methods out to run the program
+
 def input_students
   puts "Please enter the names of the students".center(50, "******")
   puts "To finish, just hit return four times".center(50, "******")
-  # creates an empty array
-  students = []
-  # gets the first names
+  # students = []
   name = gets.strip
-  # while teh name is not empty, repeat this code
   while !name.empty? do
     cohort_dates = [:january, :march, :june, :august, :november]
 
@@ -46,32 +41,64 @@ def input_students
     puts "\n" + "Please enter the height of the student in cm".center(50, "--------")
     height = gets.chomp
 
-    # add the student hash to the array
     if name.start_with?("D") && name.length < 12
-      students << {name: name, cohort: cohort, nationality: nationality, height: height }
-      if students.count == 1
-        puts "Now we have #{students.count} student"
+      @students << {name: name, cohort: cohort, nationality: nationality, height: height }
+      if @students.count == 1
+        puts "Now we have #{@students.count} student"
       else
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{@students.count} students"
       end
-    # get another name from the user
     else
       nil
     end
     puts "\n" + "Please enter the names of the student".center(50, "******")
     name = gets.chomp
   end
-  # return the array of students
-  students.sort_by do |hash|
+  @students.sort_by do |hash|
     hash[:cohort]
   end
 end
 
-students = input_students
-if !students.empty?
-  print_header
-  print(students)
-  print_footer(students)
-else
-  nil
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
 end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+@students = []
+interactive_menu
+
+# students = input_students
+# if !students.empty?
+#   print_header
+#   print(students)
+#   print_footer(students)
+# else
+#   nil
+# end
